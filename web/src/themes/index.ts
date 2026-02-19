45f9be2742c5fbefac90398f638433fdaba2977b
ccf8dh@virginia.edu <ccf8dh@virginia.edu>
2026-02-18 21:39:19 -0500

    Initialize feat-theme-changer

---
+++ web/src/themes/index.ts
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Theme } from "@mui/material";
+
+declare module "@mui/material/styles" {
+    interface Theme {
+        custom: {
+            icon: React.CSSProperties["color"];
+            loadingBar: React.CSSProperties["color"];
+        };
+    }
+
+    interface ThemeOptions {
+        custom?: {
+            icon?: React.CSSProperties["color"];
+            loadingBar?: React.CSSProperties["color"];
+        };
+    }
+}
+
+declare module "@mui/styles/defaultTheme" {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface DefaultTheme extends Theme {}
+}
+
+export const ThemeNameAuto = "auto";
+export const ThemeNameLight = "light";
+export const ThemeNameDark = "dark";
+export const ThemeNameGrey = "grey";
+export const ThemeNameOled = "oled";
+
+export { default as Light } from "@themes/Light";
+export { default as Dark } from "@themes/Dark";
+export { default as Grey } from "@themes/Grey";
+export { default as Oled } from "@themes/Oled";
